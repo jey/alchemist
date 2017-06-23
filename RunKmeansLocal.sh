79be134a24b9e1c6bc5feb6ca55bfc48519257b4
Shusen Wang <wssatzju@gmail.com>
2017-06-23 12:54:35 -0700

    test k-means clustering

---
+++ RunKmeansLocal.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+sbt -batch assembly
+
+# user specified
+PROJ_HOME="$HOME/Documents/alchemist"
+SPARK_HOME="$HOME/local/spark-2.1.1"
+MASTER="local[8]"
+CLUSTER_NUM="10"
+MAX_ITERS="100"
+
+# the rest does not need change
+
+# data
+export DATA_FILE="$PROJ_HOME/data/mnist.t"
+
+# save clustering results
+OUTPUT_DIR="$PROJ_HOME/.tmp"
+mkdir $OUTPUT_DIR
+export OUTPUT_FILE="$OUTPUT_DIR/kmeans_result"
+
+# .jar file
+JAR_FILE="$PROJ_HOME/test/target/scala-2.11/test-kmeans-assembly-0.0.2.jar"
+
+$SPARK_HOME/bin/spark-submit \
+    --master $MASTER \
+    $JAR_FILE $CLUSTER_NUM $MAX_ITERS \
+    > ResultTestKmeans.out
+
+
+# Compute NMI using a Python script
+MNI_PY="$PROJ_HOME/test/src/main/scala/amplab/alchemist/nmi.py"
+    
+python $MNI_PY -f $OUTPUT_FILE"_spark.txt" \
+    >> ResultTestKmeans.out
+    
+python $MNI_PY -f $OUTPUT_FILE"_alchemist.txt" \
+    >> ResultTestKmeans.out
