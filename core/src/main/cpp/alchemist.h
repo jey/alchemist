31825d5c0028a030e68fd9541bf584fb588c60fe
Jey Kottalam <jey@cs.berkeley.edu>
2017-07-17 14:29:04 -0700

    fix last merge

--- core/src/main/cpp/alchemist.h
+++ core/src/main/cpp/alchemist.h
@@ -18,33 +18,6 @@
 #include "endian.h"
 #include "spdlog/fmt/fmt.h"
 
-#ifdef __APPLE__
-	#include <machine/endian.h>
-	#include <libkern/OSByteOrder.h>
-	
-	#define htobe16(x) OSSwapHostToBigInt16(x)
-	#define htole16(x) OSSwapHostToLittleInt16(x)
-	#define be16toh(x) OSSwapBigToHostInt16(x)
-	#define le16toh(x) OSSwapLittleToHostInt16(x)
-	
-	#define htobe32(x) OSSwapHostToBigInt32(x)
-	#define htole32(x) OSSwapHostToLittleInt32(x)
-	#define be32toh(x) OSSwapBigToHostInt32(x)
-	#define le32toh(x) OSSwapLittleToHostInt32(x)
-	
-	#define htobe64(x) OSSwapHostToBigInt64(x)
-	#define htole64(x) OSSwapHostToLittleInt64(x)
-	#define be64toh(x) OSSwapBigToHostInt64(x)
-	#define le64toh(x) OSSwapLittleToHostInt64(x)
-	
-	#define __BIG_ENDIAN    BIG_ENDIAN
-	#define __LITTLE_ENDIAN LITTLE_ENDIAN
-	#define __BYTE_ORDER    BYTE_ORDER
-#else
-	#include <endian.h>
-#endif
->>>>>>> 32ba1e476520b94c51fcea11ce4b611ce1fc90aa
-
 #define UNLIKELY(x) __builtin_expect(!!(x), 0)
 
 #ifndef NDEBUG
